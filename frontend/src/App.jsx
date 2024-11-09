7d5ae0a353e1792d54e8c741ae8bfdc6b58b9cf9
unknown <yosefdejene747@gmail.com>
2024-11-09 13:27:42 +0300

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom"
+
+ 
+
+function App() {
+  
+
+  return (
+    <>
+    <nav>Navbar</nav>
+    <main className='min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary'>
+      <Outlet/>
+     </main>
+     <footer>footer</footer>
+    </>
+  )
+}
+
+export default App
